66d1667f74937d48d1074979d007379af4cc0891
Wang Longfei <wanglongfei@Carpe-Diem.local>
2013-01-15 15:01:27 +0800

    Initial Commit

---
+++ p4/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  p4
+//
+//  Created by Wang Longfei on 15/1/13.
+//  Copyright (c) 2013 Wang Longfei. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
